32a07b21a2fe22cc4613a50d06fbf19feb37464c
Noura Azeem <noura.azeem@gmail.com>
2016-06-04 15:18:59 -0400

    form-based workflow feature completed

--- db/migrate/20160602160017_create_articles.rb
+++ db/migrate/20160602160017_create_articles.rb
@@ -2,7 +2,7 @@ class CreateArticles < ActiveRecord::Migration
   def change
     create_table :articles do |t|
       t.string :title
-      t.text :body, null: false ##comma will seperate the two commands. Also, two spaces instead of tab
+      t.text :body
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 20160602160017) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
-    t.text     "body",       null: false
+    t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
